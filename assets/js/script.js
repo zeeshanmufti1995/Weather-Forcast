0ec8e15c0402a7683a7e859174b428284df48010
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2021-11-21 17:25:48 -0600

    check button works

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@ var inputValue = $('.search');
 var searchBtn = $('.searchBtn');
 var storedSearches = $('#storedSearches');
 var main = $('main');
+
+
+searchBtn.on('click', function(){
+    console.log("hello")
+
+})
