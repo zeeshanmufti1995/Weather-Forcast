3f604d259c04f09669302e6b46eb69d27a5c2562
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2021-11-21 18:03:02 -0600

    fetch the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,17 @@ var storedSearches = $('#storedSearches');
 var main = $('main');
 
 
+function fetchWeather(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputValue.val() + '&units=imperial&id=524901&appid=c0c8e3bb6abc081507f7c5a91f54d2e0')
+        .then(function(respone){
+            respone.json().then(function(data){
+                console.log(data);
+            })
+        })
+
+}
+
 searchBtn.on('click', function(){
-    console.log("hello")
+    fetchWeather();
 
 })
