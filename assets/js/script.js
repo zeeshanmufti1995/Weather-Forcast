104f55641ef81da931467b9cc2cb3b365c0c0c2c
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2021-11-21 19:37:07 -0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,16 @@ var searchBtn = $('.searchBtn');
 var storedSearches = $('#storedSearches');
 var main = $('main');
 
-var searches= [];
+var searches = [];
 
-
+//renders the searches
 function renderSearches() {
-  
+
     storedSearches.html("");
     for (var i = 0; i < searches.length; i++) {
         var search = searches[i];
-        
 
-        var button = $(`<button>`);
+        var button = $('<button>');
         button.text(search);
         button.attr('data-index', i);
         button.attr('class', 'list-group-item list-group-item-action');
@@ -26,85 +25,90 @@ function renderSearches() {
             inputValue.val(citySearch);
             fetchWeather();
         })
-      
     }
 }
 
+//happens on page load/refresh
+function init() {
+    var stored = JSON.parse(localStorage.getItem('searches'));
+    if (stored !== null) {
+        searches = stored;
+    }
+    renderSearches();
+}
 
 //saves searches
 function storeSearches() {
     localStorage.setItem('searches', JSON.stringify(searches));
 }
-searchBtn.on('click', function(event){
-  
+
+//adds text to array, calls fetchWeather, calls storing function and render function
+searchBtn.on('click', function(event) {
     event.preventDefault();
-    
-        var searchText = inputValue.val();
-        if (searchText === "") {
-            return;
-        }
-        searches.push(searchText);
-      
-        fetchWeather();
-        storedSearches();
-        renderSearches();
-        
-    
-    })
 
-function fetchWeather(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputValue.val() + '&units=imperial&id=524901&appid=c0c8e3bb6abc081507f7c5a91f54d2e0')
-        .then(function(respone){
-          return respone.json();
-        })
-        .then(function(data){
-                console.log(data);
-                var nameValue = data.name;
-                $('.name').text(nameValue);
-                var latitude = data.coord.lat;
-                var longitude = data.coord.lon;
+    var searchText = inputValue.val();
+    if (searchText === "") {
+        return;
+    }
+    searches.push(searchText);
 
-           
- //uses values from first API to fetch another.
- function uviFetch() {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude +'&lon=' + longitude + '&units=imperial&id=524901&appid=0fe3cfd026afb76b1605f15581136ad8')
+    fetchWeather();
+    storeSearches();
+    renderSearches();
+})
+
+//uses the input value, gets data from weather API on search button click.
+function fetchWeather() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputValue.val() + '&units=imperial&id=524901&appid=0fe3cfd026afb76b1605f15581136ad8')
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            //adds data to html
-            console.log(data);
-            var weatherDesc = data.current.weather[0].main;
-            $('.weather').text('Weather: ' + weatherDesc);
-            var dateValue = data.current.dt;
-            var newDate = new Date(dateValue*1000).toLocaleDateString('en-US');
-            $('.name').append(' (' + newDate + ')');
-            var weatherIcon = data.current.weather[0].icon;
-            $('.name').append('<img class="weatherIcon iconSizing">');
-            $('.weatherIcon').attr('src', 'http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png');
-            $('.weatherIcon').attr('alt', 'Weather Icon');
-            var tempValue = data.current.temp;
-            $('.temp').text('Temperature: ' + Math.round(tempValue) + '° F');
-            var humidityValue = data.current.humidity;
-            $('.humidity').text('Humidity: ' + humidityValue + '%');
-            var uvValue = data.current.uvi;
-            $('span').text(uvValue);
-            var windValue = data.current.wind_speed;
-            $('.wind').text('Wind Speed: ' + windValue + 'mph');
+            
+            var nameValue = data.name;
+            $('.name').text(nameValue);
+            var latitude = data.coord.lat;
+            var longitude = data.coord.lon;
 
-             //forecast tomorrow
-             var dates = data.daily[1].dt;
-             var forecastDates = new Date(dates*1000).toLocaleDateString('en-US');
-             $('#dateOne').text(forecastDates);
-             var forecastWeatherIcon = data.daily[1].weather[0].icon;
-             $('#weatherIconOne').attr('src', 'http://openweathermap.org/img/wn/' + forecastWeatherIcon + '@2x.png');
-             var forecastTemp = data.daily[1].temp.day;
-             $('#tempOne').text('Temperature: ' + Math.round(forecastTemp) + '° F');
-             var forecastHumidity = data.daily[1].humidity;
-             $('#humidityOne').text('Humidity: ' + forecastHumidity + '%');
+            //uses values from first API to fetch another.
+            function uviFetch() {
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude +'&lon=' + longitude + '&units=imperial&id=524901&appid=0fe3cfd026afb76b1605f15581136ad8')
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    //adds data to html
+                    console.log(data);
+                    var weatherDesc = data.current.weather[0].main;
+                    $('.weather').text('Weather: ' + weatherDesc);
+                    var dateValue = data.current.dt;
+                    var newDate = new Date(dateValue*1000).toLocaleDateString('en-US');
+                    $('.name').append(' (' + newDate + ')');
+                    var weatherIcon = data.current.weather[0].icon;
+                    $('.name').append('<img class="weatherIcon iconSizing">');
+                    $('.weatherIcon').attr('src', 'http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png');
+                    $('.weatherIcon').attr('alt', 'Weather Icon');
+                    var tempValue = data.current.temp;
+                    $('.temp').text('Temperature: ' + Math.round(tempValue) + '° F');
+                    var humidityValue = data.current.humidity;
+                    $('.humidity').text('Humidity: ' + humidityValue + '%');
+                    var uvValue = data.current.uvi;
+                    $('span').text(uvValue);
+                    var windValue = data.current.wind_speed;
+                    $('.wind').text('Wind Speed: ' + windValue + 'mph');
 
+                    //forecast tomorrow
+                    var dates = data.daily[1].dt;
+                    var forecastDates = new Date(dates*1000).toLocaleDateString('en-US');
+                    $('#dateOne').text(forecastDates);
+                    var forecastWeatherIcon = data.daily[1].weather[0].icon;
+                    $('#weatherIconOne').attr('src', 'http://openweathermap.org/img/wn/' + forecastWeatherIcon + '@2x.png');
+                    var forecastTemp = data.daily[1].temp.day;
+                    $('#tempOne').text('Temperature: ' + Math.round(forecastTemp) + '° F');
+                    var forecastHumidity = data.daily[1].humidity;
+                    $('#humidityOne').text('Humidity: ' + forecastHumidity + '%');
 
-              //forecast day 2
+                    //forecast day 2
                     var dates = data.daily[2].dt;
                     var forecastDates = new Date(dates*1000).toLocaleDateString('en-US');
                     $('#dateTwo').text(forecastDates);
@@ -126,18 +130,18 @@ function fetchWeather(){
                     var forecastHumidity = data.daily[3].humidity;
                     $('#humidityThree').text('Humidity: ' + forecastHumidity + '%');
 
-                     //forecast day 4
-                     var dates = data.daily[4].dt;
-                     var forecastDates = new Date(dates*1000).toLocaleDateString('en-US');
-                     $('#dateFour').text(forecastDates);
-                     var forecastWeatherIcon = data.daily[4].weather[0].icon;
-                     $('#weatherIconFour').attr('src', 'http://openweathermap.org/img/wn/' + forecastWeatherIcon + '@2x.png');
-                     var forecastTemp = data.daily[4].temp.day;
-                     $('#tempFour').text('Temperature: ' + Math.round(forecastTemp) + '° F');
-                     var forecastHumidity = data.daily[4].humidity;
-                     $('#humidityFour').text('Humidity: ' + forecastHumidity + '%');
+                    //forecast day 4
+                    var dates = data.daily[4].dt;
+                    var forecastDates = new Date(dates*1000).toLocaleDateString('en-US');
+                    $('#dateFour').text(forecastDates);
+                    var forecastWeatherIcon = data.daily[4].weather[0].icon;
+                    $('#weatherIconFour').attr('src', 'http://openweathermap.org/img/wn/' + forecastWeatherIcon + '@2x.png');
+                    var forecastTemp = data.daily[4].temp.day;
+                    $('#tempFour').text('Temperature: ' + Math.round(forecastTemp) + '° F');
+                    var forecastHumidity = data.daily[4].humidity;
+                    $('#humidityFour').text('Humidity: ' + forecastHumidity + '%');
 
-                     //forecast day 5
+                    //forecast day 5
                     var dates = data.daily[5].dt;
                     var forecastDates = new Date(dates*1000).toLocaleDateString('en-US');
                     $('#dateFive').text(forecastDates);
@@ -147,8 +151,30 @@ function fetchWeather(){
                     $('#tempFive').text('Temperature: ' + Math.round(forecastTemp) + '° F');
                     var forecastHumidity = data.daily[5].humidity;
                     $('#humidityFive').text('Humidity: ' + forecastHumidity + '%');
- 
-
-            })
-
-
+                    
+                    //changes color of UVI
+                    function uvColor() {
+                        uvIndex = $('span');
+                        if (uvValue >=0 && uvValue < 3) {
+                            uvIndex.addClass('favorable');
+                            uvIndex.removeClass('severe');
+                            uvIndex.removeClass('moderate')
+                        } else if (uvValue >= 3 && uvValue < 6) {
+                            uvIndex.addClass('moderate');
+                            uvIndex.removeClass('severe');
+                            uvIndex.removeClass('favorable');
+                        } else if (uvValue >= 6) {
+                            uvIndex.addClass('severe');
+                            uvIndex.removeClass('moderate');
+                            uvIndex.removeClass('favorable');
+                        }
+                    }
+                    uvColor();
+                    inputValue.val("");
+                    main.removeClass('hidden');
+                })
+            }
+            uviFetch();
+        })
+}
+init();
